8119b394080f3d09c865a4b4adbd803b8051121f
Paulo Gonçalves <Paulogrdc@gmail.com>
2026-06-23 16:55:53 -0300

    Fiz os métodos concretos da classe personagem atacar e receber dano

--- main/personagem.py
+++ main/personagem.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+import random
 
 class Personagem(ABC): 
     def __init__(self):
@@ -7,11 +8,22 @@ class Personagem(ABC):
         self._nivel_personagem = 1 # Protegido(#)
         self.golpes = []
 
-    def atacar(self):
-        pass
+    def atacar(self,alvo, forca = 50):
+         # Só ataca se a vida de ambos for maior que 0 
+        if self._vida > 0 and alvo._vida > 0: 
+            # gerar um golpe aleatório 
+            golpe = self.golpes[random.randrange(0,len(self.golpes))]
+            print(f"{self.nome}({self._nivel_personagem}) atacou o {alvo.nome}({alvo._nivel_personagem}) com um {golpe} de forca {forca}")
+            alvo.receber_dano(forca)
 
-    def receber_dano(self):
-        pass 
+    def receber_dano(self,dano):
+         # Gerar um numeor de 1 até a força(dano), para ver quanto de dano o alvo sofre. como se fosse um dado. 
+        fator = random.randint(1,dano)
+        self._vida -= fator
+        if self._vida <= 0: 
+            print(f"{self.nome}({self._nivel_personagem}) recebeu {fator} de dano e Morreu!")
+        else: 
+            print(f"{self.nome}({self._nivel_personagem}) recebeu dano de {fator} e ficou com {self._vida} de vida ") 
 
     @abstractmethod
     def aumentar_nivel(self): 
