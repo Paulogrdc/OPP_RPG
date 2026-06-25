3678096605dce8f41dc5b35e19a703ff3112d7a2
Paulo Gonçalves <Paulogrdc@gmail.com>
2026-06-25 15:38:10 -0300

    fiz o esboço da opção 2

--- __main__.py
+++ __main__.py
@@ -2,28 +2,37 @@ from classes.personagem import *
 from funcionalidades.função import  * 
 
 
-def main(): 
+def main(): # função principal 
+
     menu()
     navegar = int(input("Escolha uma opção: "))
 
-    match navegar: 
+    while navegar != 4: 
 
-        case 1: 
-            print("1-Guerreiro")
-            print("2-Mago")
-            try: 
-                personagem = int(input("Escolha o personagem: "))
-                if personagem == 1: 
-                    nome_personagem = str(input("Escolha um nome para o personagem: "))
-                    p1 = Guerreiro(nome_personagem)
-                    print("Personagem criado com sucesso!")
-                else: 
-                    nome_personagem = str(input("Escolha um nome para o personagem: "))
-                    p1 = Mago(nome_personagem) 
-                    print("Personagem criado com sucesso!")
-            except ValueError: 
-                print("Valor invalido! Digite o núemro 1 ou 2,respectivamente,para escolher o personagem.")
-            
+        match navegar: 
+
+            case 1: 
+                print("1-Guerreiro")
+                print("2-Mago")
+                try: 
+                    personagem = int(input("Escolha o personagem: "))
+                    if personagem == 1: 
+                        nome_personagem = str(input("Escolha um nome para o Guerreiro: "))
+                        p1 = Guerreiro(nome_personagem)
+                        print("Personagem criado com sucesso!")
+                        menu()
+                        navegar = int(input("Escolha uma opção: "))
+                    else: 
+                        nome_personagem = str(input("Escolha um nome para o Mago: "))
+                        p1 = Mago(nome_personagem) 
+                        print("Personagem criado com sucesso!")
+                except ValueError: 
+                    print("Valor invalido! Digite o núemro 1 ou 2,respectivamente,para escolher o personagem.")
+            case 2: 
+                print(f"O {nome_personagem} tem {p1._vida} pontos de vida e seu nivel é {p1._nivel_personagem}")
+                menu()
+                navegar = int(input("Escolha uma opção: "))
+                
             
 
 if __name__ == "__main__":
--- funcionalidades/função.py
+++ funcionalidades/função.py
@@ -1,5 +1,5 @@
 def menu(): 
-    print(" Digite 1 para escolher o personagem")
+    print(" Digite 1 para criar personagem")
     print(" Digite 2 para ver o status do personagem")
     print(" Digite 3 para batalhar")
     print(" Digite 4 para sair do jogo")
